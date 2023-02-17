849e62c09ab35f72be901657e7393e5ea80cee13
gintarevk <gintare.v.k@gmail.com>
2023-02-17 11:50:17 +0200

    Add new Arithmetic class

---
+++ src/main/java/lt/gintare/demo/Arithmetic.java
@@ -0,0 +1,13 @@
+package lt.gintare.demo;
+
+public class Arithmetic {
+
+    public static int sum(int a, int b) {
+        return a + b;
+
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
