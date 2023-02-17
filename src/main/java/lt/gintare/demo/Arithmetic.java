c7072b3dd89678eb72575e81dd68a5aae981636f
gintarevk <gintare.v.k@gmail.com>
2023-02-17 12:28:23 +0200

    Add greeting method into Arithmetic

--- src/main/java/lt/gintare/demo/Arithmetic.java
+++ src/main/java/lt/gintare/demo/Arithmetic.java
@@ -4,10 +4,17 @@ public class Arithmetic {
 
     public static int sum(int a, int b) {
         return a + b;
-
     }
 
     public static int subtract(int a, int b) {
         return a - b;
     }
+
+    public static String greetings(String name) {
+        return "Hello my dear friend " + name;
+    }
 }
+
+
+
+
