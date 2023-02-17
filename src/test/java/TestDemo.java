0ca89fc3e6afd6dee1f73da9acc0733fb4ab3e81
gintarevk <gintare.v.k@gmail.com>
2023-02-17 10:42:52 +0200

    Add empty tests into TestDemo class

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,2 +1,16 @@
+import org.testng.annotations.Test;
+
 public class TestDemo {
+
+    @Test
+    public void test01() {
+
+
+    }
+
+    //spaudziam @Test
+    @Test
+    public void test02(){
+
+    }
 }
