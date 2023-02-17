f57619f68802d460100bcc9a7ef9a6214b842300
gintarevk <gintare.v.k@gmail.com>
2023-02-17 17:06:45 +0200

    Add new SeleniumDemo class with demo02 method

--- src/test/java/lt/gintare/demo/SeleniumDemo.java
+++ src/test/java/lt/gintare/demo/SeleniumDemo.java
@@ -1,9 +1,13 @@
 package lt.gintare.demo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class SeleniumDemo {
 
@@ -21,5 +25,31 @@ public class SeleniumDemo {
         }
         driver.quit();
 
+    }
+
+    @Test
+    public void demo02() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        // chromeOptions.addArguments("--start-maximized ");
+        chromeOptions.addArguments("--window-size=7000, 3000");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        Duration duration = Duration.ofSeconds(10);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.vilniuscoding.lt/");
+
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        driver.quit();
     }
 }
