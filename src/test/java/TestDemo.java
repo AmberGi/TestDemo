fb83e063755f66f13fd70ff68cfa837b59684b63
gintarevk <gintare.v.k@gmail.com>
2023-02-17 11:04:10 +0200

    Rename test and implement testIfNumber255DivideWith5

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,16 +1,34 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestDemo {
 
     @Test
-    public void test01() {
+    public void testIfNumber255DividesWith5() {
+        // ko reikia testui
+        // 1. Paduoti duomenis
+        // 2. Testo veiksmai
+        // 3. Rezultatu palyginimas
+        // testas: patikrinti ar skaicius dalinasi is 5?
 
+        // 1. Duomenys:
 
+        int input = 255;
+        int actualResult;
+        int expectedResult = 0; //tikimes, kad dalinant su liekana, liekana bus 0
+
+        // 2.Testo veiksmai:
+
+        actualResult = input % 5;
+
+        // 3. Palyginimas:
+
+        Assert.assertEquals(actualResult, expectedResult);
     }
 
     //spaudziam @Test
     @Test
-    public void test02(){
+    public void test02() {
 
     }
 }
